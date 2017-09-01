ec716dce3326a6dcdd60ed92abc86c032c5b0922
alcazar90 <alcazar.cristobal90@gmail.com>
2017-09-01 01:23:41 -0300

    functions to implement recursive binary splitting

--- recursive_binary_splitting.R
+++ recursive_binary_splitting.R
@@ -11,6 +11,20 @@ df <- df[!is.na(df$Salary), ]
 x <- df$Years
 y <- df$Salary
 
+split_data <- function(X, var, val) {
+  # Input: X, a data frame of the predictor variables 
+  #   var, the name of the variable in which the data will be split
+  #   val, the value in which the data will be separate
+  # Return a list of two elements. The first one is the portion given by
+  # the left region of the cut point value ("val"), and the second, is 
+  # the right region of the cut point valueEl primero es la porción
+  # (FOR NOW THIS IMPLEMENTATION ONLY WORK WITH CONTINUOS VARIABLES
+  # AS RESPONSE AND PREDICTORS)
+  subset_vector <- X[[var]] < val
+  left_region <- X[subset_vector, ]
+  right_region <- X[!subset_vector, ]
+  list(r1 = left_region, r2 = right_region)
+}
 
 
 cut_points <- function(x) {
@@ -28,12 +42,12 @@ cut_points <- function(x) {
   s
 }
 
-
-
-find_split <- function(y, x, s) {
-  #
-  #
-  #
+optimal_point <- function(y, x, s) {
+  # Input: y, response variable
+  #   x, the predictor variable to split
+  #   s, cut-points to evaluate
+  # Return the optimal cut point, this is the point that minimize
+  # the cost function (RSS of both region)
   output <- vector("double", length(s))
   for (i in 1:length(s)) {
     half_plane <- x > s[i]
@@ -47,8 +61,34 @@ find_split <- function(y, x, s) {
     output[i] <- RSST
     names(output)[i] <- s[i]
   }
-  output
+  list(metric = min(output), cut_point = as.double(names(which.min(output))))
 }
+
+optimal_point(y, x, cut_points(x))
+
+
+select_var <- function(y, X) {
+  # Input: y, the response variable
+  #  X, the predictor variables
+  # Return the selected variable, the variable with the minimum RSS and
+  # the respectively split point
+  metric <- vector("double", ncol(X))
+  cp_star <- vector("double", ncol(X))
+  for (i in 1:ncol(X)) {
+    # so for each variable we need the minimal metric and the optimal
+    # cut-point that produce the minimal metric
+    temp <- optimal_point(y, X[[i]], cut_points(X[[i]]))
+    metric[i] <- temp$metric
+    cp_star[i] <- temp$cut_point
+  }
+  list(var = names(X)[which.min(metric)], split_point = cp_star[which.min(metric)])
+}
+
+t <- select_var(y, df[, c(2, 3)])
+split_data(df, t$var, t$split_point)
+
+
+
 
 # usage
 output <- find_split(y, x, cut_points(x))
